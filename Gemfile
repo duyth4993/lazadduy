38bea5ebfc816b46abf359a51ccb00686af4cc75
duyth4993 <duy.th4993@gmail.com>
2016-02-11 01:35:51 +0700

    Authenticate with devise

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "turbolinks"
 gem "jbuilder", "~> 2.0"
 gem "sdoc", "~> 0.4.0", group: :doc
 gem "config"
+gem "devise"
 
 group :development, :test do
   gem "byebug", "3.4.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: "auth", path_names: {sign_in: "login", sign_out: "logout"}
   root "static_pages#home"
   get "help" => "static_pages#help"
 end
